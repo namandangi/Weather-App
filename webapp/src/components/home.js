f0bf2db9088be09808e66a07ec16a8275fbe017a
namandangi <danginaman55@gmail.com>
2020-04-13 15:56:10 +0530

    add autocomplete cities :rocket:

--- webapp/src/components/home.js
+++ webapp/src/components/home.js
@@ -1,9 +1,29 @@
 import React,{Component} from 'react';
 import  './home.css';
 import logo from '../logo.svg';
+import i01d from './icons/i01d.png';
+import i02d from './icons/i02d.png';
+import i03d from './icons/i03d.png';
+import i04d from './icons/i04d.png';
+import i09d from './icons/i09d.png';
+import i10d from './icons/i10d.png';
+import i11d from './icons/i11d.png';
+import i13d from './icons/i13d.png';
+import i50d from './icons/i50d.png';
+import i01n from './icons/i01n.png';
+import i02n from './icons/i02n.png';
+import i03n from './icons/i03n.png';
+import i04n from './icons/i04n.png';
+import i09n from './icons/i09n.png';
+import i10n from './icons/i10n.png';
+import i11n from './icons/i11n.png';
+import i13n from './icons/i13n.png';
+import i50n from './icons/i50n.png';
 
 import {Link} from 'react-router-dom';
-import {AiOutlineLeft} from 'react-icons/ai';
+import {AiOutlineLeft,AiOutlineRight} from 'react-icons/ai';
+import {MdHistory} from 'react-icons/md';
+import AlgoliaPlaces from 'algolia-places-react';
 
 export default class Home extends Component{
 	state={
@@ -20,8 +40,9 @@ export default class Home extends Component{
 		humidity:'',
 		pressure:'',
 		wind:'',
-		date: new Date().toLocaleString()
-	}
+		date: '',
+		icon:logo
+	}	
 
 	 handleChange = async(e)=> {
       //console.dir(e.target.value);
@@ -55,19 +76,29 @@ export default class Home extends Component{
     //     console.dir("Error Code = " + error.code + " - " + error.message);
     //   }
     // );
-    //console.dir(this.props.location.state.city)		
+    //console.dir(this.props.location.state.city)
+    var icons= {
+		'01d':i01d,'02d':i02d,'03d':i03d,'04d':i04d,'09d':i09d,'10d':i10d,'11d':i11d,'13d':i13d,'50d':i50d,'01n':i01n,'02n':i02n,'03n':i03n,'04n':i04n,'09n':i09n,'10n':i10n,'11n':i11n,'13n':i13n,'50n':i50n
+	}
     if(this.props.location.state!==undefined)
     await this.setState({city:this.props.location.state.city});
-		console.dir(this.state.data,this.props.location.state)
+		console.dir(this.props.location.state)
 		if(this.state.city!==''){
 			const response = await fetch('http://localhost:8000/api/weather/search',
 				{
 					method:'POST',
-					body:JSON.stringify({city:this.state.city}),
+					body:JSON.stringify({city:this.props.location.state.city}),
 					 headers: { 'Content-type': 'application/json' ,
-					 'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTkxZjMzMmU0ZDRjNjE5ZDk1MjE2OTAiLCJpYXQiOjE1ODY2MjMyODIsImV4cCI6MTU4NjcwOTY4Mn0.yfB98aximoGvJ3TyottcqMoQUsVw5I740XArJ3b9n9Y'}
+					 'Authorization': 'Bearer ' + this.props.location.state.token}
 				});
 		  const content = await response.json();
+		  if(content!=null){
+		  console.dir(response,content);
+		  let date = new Date();
+		  let month = date.toLocaleString('default',{month:'long'});
+		  let day = date.toLocaleString('default',{weekday:'long'});
+		  let currdate = date.getDate();
+		  let tempDate = day+' '+currdate+' '+month;
 		  let temp_temp = await Math.round(Number(content.main.temp)-273);
 		  let temp_min = await Math.round(Number(content.main.temp_min)-273);
 		  let temp_max = await Math.round(Number(content.main.temp_max)-273);
@@ -77,12 +108,20 @@ export default class Home extends Component{
 		  let temp_weather = content.weather[0].main;
 		  let temp_sunrise = await new Date(content.sys.sunrise*1000).toUTCString().slice(-11,-7);
 		  let temp_sunset = await new Date(content.sys.sunset*1000).toUTCString().slice(-11,-7); 
-		  await this.setState({data:content,country:content.sys.country,temp:temp_temp,weather:temp_weather,sunrise:temp_sunrise,sunset:temp_sunset,maxt:temp_max,mint:temp_min,humidity:temp_humidty,pressure:temp_pressure,wind:temp_wind});
+		  let icon;
+		  if(content.weather[0].icon in icons)
+		  	 icon = icons[content.weather[0].icon]
+		  await this.setState({icon:icon,date:tempDate,data:content,country:content.sys.country,temp:temp_temp,weather:temp_weather,sunrise:temp_sunrise,sunset:temp_sunset,maxt:temp_max,mint:temp_min,humidity:temp_humidty,pressure:temp_pressure,wind:temp_wind});
+		  console.dir(this.state)
+		}
 		 // console.dir(response,content,this.state.data.main);
 		 //console.dir(history);
 		}
 	}
 	async componentDidUpdate(prevProps,prevState){
+		var icons= {
+		'01d':i01d,'02d':i02d,'03d':i03d,'04d':i04d,'09d':i09d,'10d':i10d,'11d':i11d,'13d':i13d,'50d':i50d,'01n':i01n,'02n':i02n,'03n':i03n,'04n':i04n,'09n':i09n,'10n':i10n,'11n':i11n,'13n':i13n,'50n':i50n
+	}
 		if(prevState.city!=this.state.city){
 			this.props = prevProps;
 			console.dir(this.props);
@@ -96,6 +135,11 @@ export default class Home extends Component{
 		  let content = await response.json();
 		  content = content.name!=="Error"?content:prevState.data;
 		  console.dir(content.name,prevState.data)
+		  let date = new Date();
+		  let month = date.toLocaleString('default',{month:'long'});
+		  let day = date.toLocaleString('default',{weekday:'long'});
+		  let currdate = date.getDate();
+		  let tempDate = day+' '+currdate+' '+month;
 		  let temp_temp = await Math.round(Number(content.main.temp)-273);
 		  let temp_min = await Math.round(Number(content.main.temp_min)-273);
 		  let temp_max = await Math.round(Number(content.main.temp_max)-273);
@@ -105,30 +149,64 @@ export default class Home extends Component{
 		  let temp_weather = content.weather[0].main;
 		  let temp_sunrise = await new Date(content.sys.sunrise*1000).toUTCString().slice(-11,-7);
 		  let temp_sunset = await new Date(content.sys.sunset*1000).toUTCString().slice(-11,-7); 
-		  await this.setState({data:content,country:content.sys.country,temp:temp_temp,weather:temp_weather,sunrise:temp_sunrise,sunset:temp_sunset,maxt:temp_max,mint:temp_min,humidity:temp_humidty,pressure:temp_pressure,wind:temp_wind});
+		  let icon;
+		  if(content.weather[0].icon in icons)
+		  	 icon = icons[content.weather[0].icon]
+		  await this.setState({icon:icon,date:tempDate,data:content,country:content.sys.country,temp:temp_temp,weather:temp_weather,sunrise:temp_sunrise,sunset:temp_sunset,maxt:temp_max,mint:temp_min,humidity:temp_humidty,pressure:temp_pressure,wind:temp_wind});
 		  //console.dir(this.state,history);	
 		}
 	}
 	render(){
 		return(
 			<>
-			<Link to={{pathname:"/",state:{token:this.props.location.state.token}}} ><a href="#"> <AiOutlineLeft /> </a></Link>
+			<Link to={{pathname:"/",state:{name:this.props.location.state.name,city:this.props.location.state.city,token:this.props.location.state.token}}} ><a id="bck" > <AiOutlineLeft /> </a></Link>
+			<Link to={{pathname:"/history",state:{name:this.props.location.state.name,city:this.state.city,token:this.props.location.state.token}}} ><a id="history" > <MdHistory /> </a></Link>
 			<br /><br />
 			<div className="container">
 			<form className="searchBar">
-				<input 	className="search" type="text" placeholder="Enter City" onClick={(e)=>{e.preventDefault();}} onSubmit={this.handleSubmit} onKeyDown={this.keyPress} onChange={this.handleChange} value={this.state.value}/>
+{/*				<input 	className="search" type="text" placeholder="Enter City" onClick={(e)=>{e.preventDefault();}} onSubmit={this.handleSubmit} onKeyDown={this.keyPress} onChange={this.handleChange} value={this.state.value}/>		*/}
+	<AlgoliaPlaces
+      placeholder='Write an address here'
+      onKeyDown={this.keyPress}
+ 
+      options={{
+        appId: 'pl3TYBGRZOBF',
+        apiKey: 'c49c5b63313e3980a6c229d2359136e0',
+        type: 'city',
+        // Other options from https://community.algolia.com/places/documentation.html#options
+      }}
+ 
+      onChange={({ query, rawAnswer, suggestion, suggestionIndex }) =>{       	
+        this.setState({city:suggestion.name})
+        console.dir('Fired when suggestion selected in the dropdown or hint was validated.',suggestion.name,this.state.city)}}
+ 
+      onSuggestions={({ rawAnswer, query, suggestions }) => 
+        console.dir('Fired when dropdown receives suggestions. You will receive the array of suggestions that are displayed.')}
+ 
+      onCursorChanged={({ rawAnswer, query, suggestion, suggestonIndex }) => 
+        console.dir('Fired when arrows keys are used to navigate suggestions.')}
+ 
+      onClear={() => 
+        console.dir('Fired when the input is cleared.')}
+ 
+      onLimit={({ message }) => 
+        console.dir('Fired when you reached your current rate limit.')}
+ 
+      onError={({ message }) => 
+        console.dir('Fired when we could not make the request to Algolia Places servers for any reason but reaching your rate limit.')}
+    />
 				<button type="submit" style={{visibility:"hidden"}}></button>
 			</form>
 			<div className="infoCard">
 				<div className="upperCard">
 					<h2>{this.state.data.name} , {this.state.country}</h2>
-					<h4>Friday 10 April
+					<h4>
 						{this.state.date}
 					</h4>
 				</div>
 				<div className="data">
 				<div className="bottomLeft">
-					<div className="imgbox"><img src={logo} style={{width:370,height:300}}/></div>
+					<div className="imgbox"><img src={this.state.icon} style={{width:370,height:300}}/></div>
 					<div className="infoText">
 						<h3>{this.state.temp}°</h3>
 						<h4>{this.state.weather}</h4>
--- webapp/src/components/landing.js
+++ webapp/src/components/landing.js
@@ -1,22 +1,30 @@
 import React,{Component} from 'react';
 import './landing.css';
 import {Link}  from 'react-router-dom';
+import AlgoliaPlaces from 'algolia-places-react';
 
 export default class Landing extends Component{
 	state={
 		value:'',
 		city:'',
-		token:''
+		token:'',
+		name:'',
+		loggedin:false
 	}
 	async componentDidMount(){
-		console.dir(this.props);
-		await this.setState({token:this.props.location.state.token});
-		console.dir("token is : ",this.props.location.state.token);
+		console.dir(this.props,this.state.loggedin);
+		if(this.props.location.state!=null){
+			await this.setState({loggedin:true,name:this.props.location.state.name,token:this.props.location.state.token});
+			console.dir("token is : ",this.props.location.state.token);
+		}
 	}
 		 handleChange = async(e)=> {
       //console.dir(e.target.value);
       await this.setState({value:e.target.value});
       console.dir(this.state.value);
+   }
+   componentDidUpdate = async(prevProps,prevState)=>{
+
    }
  // static async getDerivedStateFromProps(props,state){
  //   		// await this.setState({token:this.props.location.state.token});
@@ -33,22 +41,59 @@ export default class Landing extends Component{
    }
    handleSubmit = async (e)=>{
    	
-   	if(e.keyCode===13){
+   	if(e.keyCode===13)
+   	{
    		e.preventDefault();
    		await this.setState({city:e.target.value});
    		console.dir("city is",this.state.city);
    	}
    }
+   handleLogout= async(e)=>{
+   	await this.setState({token:'',name:'',loggedin:false});
+   	console.dir("logged out",this.state.loggedin);
+   }
 
 	render(){
 		return(
 			<>		
-			<div className="container">
+			<div className="Navbar">{this.state.name==''?<Link to='/login'><a href="#">Login | Register</a></Link>:<><h3>Logged in as {this.state.name} | </h3><h3 id="logout" onClick={this.handleLogout}>&nbsp;Logout</h3></>}</div>			
+			<div className="Container">
 			<h1>WEATHER APP</h1>
 			<br /><br />
 			<form className="searchBar">
-				<input className="search" type="text" placeholder="Enter City" onClick={(e)=>{e.preventDefault();}}onSubmit={this.handleSubmit} onKeyDown={this.keyPress} onChange={this.handleChange} value={this.state.value}/>
-				<Link to={{pathname:"/weather",state:{city:this.state.value,token:this.state.token}}}> <button type="submit" style={{visibility:"hidden"}}></button></Link>
+			{console.dir(this.state.loggedin)}
+				{/* <input className="search" type="text" placeholder="Enter City" onClick={(e)=>{e.preventDefault();}}onSubmit={this.handleSubmit} onKeyDown={this.keyPress} onChange={this.handleChange} />	*/}
+	<AlgoliaPlaces
+      placeholder='Write an address here'
+      onSubmit={this.handleSubmit}
+ 
+      options={{
+        appId: 'pl3TYBGRZOBF',
+        apiKey: 'c49c5b63313e3980a6c229d2359136e0',
+        type: 'city',
+        // Other options from https://community.algolia.com/places/documentation.html#options
+      }}
+ 
+      onChange={({ query, rawAnswer, suggestion, suggestionIndex }) =>{       	
+        this.setState({city:suggestion.name})
+        console.dir('Fired when suggestion selected in the dropdown or hint was validated.',suggestion.name,this.state.city)}}
+ 
+      onSuggestions={({ rawAnswer, query, suggestions }) => 
+        console.dir('Fired when dropdown receives suggestions. You will receive the array of suggestions that are displayed.')}
+ 
+      onCursorChanged={({ rawAnswer, query, suggestion, suggestonIndex }) => 
+        console.dir('Fired when arrows keys are used to navigate suggestions.')}
+ 
+      onClear={() => 
+        console.dir('Fired when the input is cleared.')}
+ 
+      onLimit={({ message }) => 
+        console.dir('Fired when you reached your current rate limit.')}
+ 
+      onError={({ message }) => 
+        console.dir('Fired when we could not make the request to Algolia Places servers for any reason but reaching your rate limit.')}
+    />
+				{this.state.loggedin?<Link to={{pathname:"/weather",state:{name:this.state.name,city:this.state.city,token:this.state.token}}}> <button type="submit" style={{visibility:"hidden"}}></button></Link>:<Link to='/'> <button type="submit" style={{visibility:"hidden"}}></button></Link>}
 			</form>
 			</div>
 			</>
