e4167585d5974abc8087db68884fafc2d128c501
namandangi <danginaman55@gmail.com>
2020-10-29 17:28:10 +0530

    refactor history page

--- webapp/src/components/history.js
+++ webapp/src/components/history.js
@@ -1,4 +1,6 @@
 import React,{Component} from 'react';
+import { Link } from 'react-router-dom';
+import { AiOutlineLeft } from 'react-icons/ai';
 import logo from '../logo.svg';
 import i01d from './icons/i01d.png';
 import i02d from './icons/i02d.png';
@@ -18,44 +20,55 @@ import i10n from './icons/i10n.png';
 import i11n from './icons/i11n.png';
 import i13n from './icons/i13n.png';
 import i50n from './icons/i50n.png';
-
-import {Link} from 'react-router-dom';
-import {AiOutlineLeft} from 'react-icons/ai';
-
 export default class History extends Component{
-	state={
-			token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTkzNWE5NTgwYWVkOTVkMDM2NmM5OWIiLCJpYXQiOjE1ODY3MTUyODUsImV4cCI6MTYxODI1MTI4NX0.Q9ETx8QjI865DOwUI4bq3cN8UNaIdjYiq6ohDvNaifk',
-			data:[],
-			icon:[]
+	state = {
+			token: '',
+			data: [],
+			icon: []
 		}
-		async componentWillMount(){
-	var icons= {
-		'01d':i01d,'02d':i02d,'03d':i03d,'04d':i04d,'09d':i09d,'10d':i10d,'11d':i11d,'13d':i13d,'50d':i50d,'01n':i01n,'02n':i02n,'03n':i03n,'04n':i04n,'09n':i09n,'10n':i10n,'11n':i11n,'13n':i13n,'50n':i50n
+	constructor() {
+		super();
+		this.iconList = {
+			'01d': i01d, '02d': i02d, '03d': i03d, '04d': i04d, '09d': i09d,
+			'10d': i10d, '11d': i11d, '13d': i13d, '50d': i50d, '01n': i01n,
+			'02n': i02n, '03n': i03n, '04n': i04n, '09n': i09n, '10n': i10n,
+			'11n': i11n, '13n': i13n, '50n': i50n
+		};		
+	}	 
+	loadData = async() => {
+		const response = await fetch('/api/weather/history', {
+			method:'GET',
+			headers: { 
+				'Content-type': 'application/json' ,
+				'Authorization': 'Bearer ' + this.props.location.state.token
+				}
+		});
+		const content = await response.json();
+		let tempdata = [];
+		let tempicon = [];
+		content.map((el, id) => {
+			tempdata[id] = el;
+			tempicon[id] = this.iconList[el.weather[0].icon] || logo;
+		})
+		this.setState({ data: tempdata, icon: tempicon });
 	}
-			const response = await fetch('http://localhost:8000/api/weather/history',{
-				method:'GET',
-				headers: { 'Content-type': 'application/json' ,
-					 'Authorization': 'Bearer ' + this.props.location.state.token}
-			})	;
-			const content = await response.json();
-			let tempdata = []
-			let tempicon = []
-			content.map((el,id)=>{
-				tempdata[id] = el;
-				tempicon[id] = icons[el.weather[0].icon]||logo;
-			})
-			await this.setState({data:tempdata,icon:tempicon});
-			console.dir(this.state)
-		}
-	render(){		
+	componentWillMount() {
+		this.loadData();
+	}
+
+	render() {		
 		return(
 			<>			
-			<div id="back" style={{width:'0'}}><Link to={{pathname:"/weather",state:{name:this.props.location.state.name,city:this.props.location.state.city,token:this.props.location.state.token}}} ><a > <AiOutlineLeft /> </a></Link></div>
-			<br /><br />
-			<h1 style={{fontSize:50,fontWeight:600,color:'white'}}>HISTORY</h1>
-			{this.state.data.map((el,id)=>
-			<div className="container">
-
+			<div className="nav">
+				<div className="bck">
+					<Link to={{ pathname:"/", state: { name: this.props.location.state.name, city: this.props.location.state.city, token: this.props.location.state.token }}} >
+						<AiOutlineLeft />
+					</Link>
+				</div>
+			</div>
+			<h1 style={{ fontFamily: "Helvetica, sans-serif" ,fontSize: 50, fontWeight: 600, color: 'white' }}>HISTORY</h1>
+			{this.state.data.map((el, id) =>
+			<div className="container" style={{ marginTop: 0}}>
 			<div className="infoCard">
 				<div className="upperCard">
 					<h2>{el.name} , {el.sys.country}</h2>
@@ -65,7 +78,9 @@ export default class History extends Component{
 				</div>
 				<div className="data">
 					<div className="bottomLeft">
-						<div className="imgbox"><img src={this.state.icon[id]} style={{width:370,height:300}}/></div>
+						<div className="imgbox">
+							<img src={this.state.icon[id]} style={{ width: 370, height: 300 }}/>
+						</div>
 						<div className="infoText">
 							<h3>{Math.round(Number(el.main.temp-273))}°</h3>
 							<h4>{el.weather[0].main}</h4>
