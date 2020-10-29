d8745b9b4b373e3a48817e731a543e489a5c03ea
namandangi <danginaman55@gmail.com>
2020-10-29 16:36:52 +0530

    refactor models

--- models/history.js
+++ models/history.js
@@ -1,42 +1,42 @@
 const mongoose = require('mongoose');
 
 var HistorySchema = mongoose.Schema({
-	user:{
+	user: {
 		 type: mongoose.Schema.Types.ObjectId,
 		 ref:'User'
 	},
-	coord:{
-		lon:Number,
-		lat:Number
+	coord: {
+		lon: Number,
+		lat: Number
 	},
-	weather:[{
-		main:String,
-		description:String,
-		icon:String
+	weather: [{
+		main: String,
+		description: String,
+		icon: String
 	}],
-	main:{
-		temp:Number,
-		feels_like:Number,
-		temp_min:Number,
-		temp_max:Number,
-		pressure:Number,
-		humidity:Number,
-		sea_level:Number,
-		grnd_level:Number
+	main: {
+		temp: Number,
+		feels_like: Number,
+		temp_min: Number,
+		temp_max: Number,
+		pressure: Number,
+		humidity: Number,
+		sea_level: Number,
+		grnd_level: Number
 	},
-	wind:{
+	wind: {
 		speed:Number
 	},
-	sys:{
-		country:String,
-		sunrise:Number,
-		sunset:Number
+	sys: {
+		country: String,
+		sunrise: Number,
+		sunset: Number
 	},
-	name:String,
-	timezone:Number,
-	searchedAt:{
-		type:String
+	name: String,
+	timezone: Number,
+	searchedAt: {
+		type: String
 	}
 });
 
-module.exports = mongoose.model("History",HistorySchema);
+module.exports = mongoose.model("History", HistorySchema);
--- models/user.js
+++ models/user.js
@@ -1,23 +1,25 @@
 const mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
-	name:{
-		type:String,
-		required:true,
-		trim:true
+	name: {
+		type: String,
+		required: true,
+		trim: true
 	},
-	email :{
-		type:String,
-		required:true,
-		unique:true,
-		trim:true,
-		lowercase:true
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+		lowercase: true
 	},
-	password:{
-		type:String,
-		required:true
+	password: {
+		type: String,
+		required: true
 	},
-	token:{type:String}
+	token: {
+		type: String
+	}
 });
 
-module.exports = mongoose.model("User",UserSchema);
+module.exports = mongoose.model("User", UserSchema);
